aaefd411328f2aedc9367fa70f70083be05eac86
feralmark <76614048+feralmark@users.noreply.github.com>
2023-12-06 15:40:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-$(document).ready(function(){
-  $('#searchInput').on('keyup', function() {
-    var searchText = $(this).val().toLowerCase();
+// Function to fetch and parse YAML files
+function fetchDataFromYAML(filename) {
+  return fetch(`yaml/${filename}`)
+    .then(response => response.text())
+    .then(text => jsyaml.load(text));
+}
 
-    $('#myTable tbody tr').filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(searchText) > -1);
+// Function to populate the table with YAML data
+function populateTable(data) {
+  const tableBody = document.querySelector('#myTable tbody');
+  tableBody.innerHTML = ''; // Clear existing table content
+
+  data.forEach(row => {
+    const newRow = document.createElement('tr');
+    Object.values(row).forEach(value => {
+      const cell = document.createElement('td');
+      cell.textContent = value;
+      newRow.appendChild(cell);
     });
+    tableBody.appendChild(newRow);
   });
-});
+}
+
+// Usage
+fetchDataFromYAML('data1.yaml') // Replace 'data1.yaml' with your YAML file name
+  .then(data => {
+    // Once data is fetched, populate the table
+    populateTable(data);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
